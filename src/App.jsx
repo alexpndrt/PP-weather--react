429b181dbc74062d0016ff0ac6263b553bab8b00
Alex PONDART <alex.pondart@oclock.school>
2025-06-07 17:28:35 +0200

    Add content static App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-function App() {
- 
-  return (
+import loader from "./assets/loader.svg";
 
-      <main>
-      
-      </main>
-  
+function App() {
+  return (
+    <main>
+      <div className="loader-container">
+        <img src={loader} alt="loading icon" />
+      </div>
+      <p className="city-name">Paris</p>
+      <p className="country-name">France</p>
+      <p className="temperature">20°C</p>
+      <div className="info-icon-container">
+        <img src="../public/icons/01d.svg" alt="weather icon" />
+      </div>
+    </main>
   );
 }
 
