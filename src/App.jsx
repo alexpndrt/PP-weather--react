99aeb31c3d6140be6653c1a202646f2e593cc4d7
Alex PONDART <alex.pondart@oclock.school>
2025-06-07 18:43:36 +0200

    feat: add error handling App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { useEffect, useState } from "react";
 
 import loader from "./assets/loader.svg";
+import browser from "./assets/browser.svg";
 import "./App.css";
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
+  const [errorInfo, setErrorInfo] = useState(null);
 
   useEffect(() => {
-    fetch(`http://api.airvisual.com/v2/nearest_city?key=${API_KEY}`)
+    fetch(`http://api.airvisual.com/v2/nearest_cit?key=${API_KEY}`)
       .then((response) => {
         console.log("Response received from API:", response);
+        // 400 - 499 : Erreur clients
+        // 500 - 599 : Erreur serveur
+        if (!response.ok)
+          throw new Error(`Error ${response.status}, ${response.statusText}`);
         return response.json();
       })
       .then((responseData) => {
@@ -21,12 +27,19 @@ function App() {
           iconId: responseData.data.current.weather.ic,
           temperature: responseData.data.current.weather.tp,
         });
+      })
+      .catch((err) => {
+        console.log("Error fetching data from API:", err);
+        console.dir(err);
+        setErrorInfo(err.message);
       });
   }, []);
 
   return (
     <main>
-      <div className={`loader-container ${!weatherData && "active"}`}>
+      <div
+        className={`loader-container ${!weatherData && !errorInfo && "active"}`}
+      >
         <img src={loader} alt="loading icon" />
       </div>
 
@@ -42,6 +55,13 @@ function App() {
               alt="weather icon"
             />
           </div>
+        </>
+      )}
+
+      {errorInfo && !weatherData && (
+        <>
+          <p className="error-information">{errorInfo}</p>
+          <img src={browser} alt="error icon" />
         </>
       )}
     </main>
