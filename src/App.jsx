0126fb37db8d4d75c09228a76275d59259aa9e1b
Alex PONDART <alex.pondart@oclock.school>
2025-06-07 18:06:56 +0200

    feat => data Weather with fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
 import loader from "./assets/loader.svg";
 import "./App.css";
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://api.airvisual.com/v2/nearest_city?key=${API_KEY}`)
+      .then((response) => {
+        console.log("Response received from API:", response);
+        return response.json();
+      })
+      .then((responseData) => {
+        console.log("Data received from API:", responseData);
+        setWeatherData({
+          city: responseData.data.city,
+          country: responseData.data.country,
+          iconId: responseData.data.current.weather.ic,
+          temperature: responseData.data.current.weather.tp,
+        });
+      });
+  }, []);
+
   return (
     <main>
       <div className="loader-container">
